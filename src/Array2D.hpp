ab22ed10959251620974426d421e31d67cfa49d8
Yilin Yang <yiliny@umich.edu>
2017-08-24 03:25:40 -0400

    Add headers for Array2D and Matrix
    Add doxygen-documented headers for Array2D and Matrix. These files
    contain detailed explanations of what their functions should do, as well
    as explanations of some essential programming concepts (like memory
    leaks and the value of procedural abstraction).

---
+++ src/Array2D.hpp
@@ -0,0 +1,210 @@
+#ifndef MAAV_PROJECT_3_ARRAY_2D_HPP
+#define MAAV_PROJECT_3_ARRAY_2D_HPP
+
+#include <utility> 	// std::pair
+
+/**
+ * @brief Represents a two-dimensional array.
+ * @author Your Name (youruniqname)
+ * @detail Though C++ does natively support two-dimensional arrays, they're
+ * 		less efficient than one-dimensional arrays in several ways.
+ *
+ *		2D C++ arrays are actually 1D arrays of pointers to 1D arrays of
+ *		data values: the "row" is the specific 1D array of values you want to
+ *		index, and the "column" is the value within that array that you want.
+ *
+ *		This leads to inefficiency.
+ *
+ *		All together, the "inner" arrays representing each row take up as much
+ *		space as an equivalent 1D array containing all of the 2D array's
+ *		elements. The "outer" array of pointers to those row arrays uses up
+ *		`{NUM_ROWS}` additional bytes of memory on top of that. When working
+ *		with very large arrays--or a very large number of small arrays--that
+ *		wasted storage space is a major concern.
+ *
+ *		"Following" those pointers to the row arrays is also an operation that
+ *		takes time. Again, this is negligible when working with small numbers
+ *		of small arrays, but it could slow down program execution substantially
+ *		when working with very large arrays or large numbers of small arrays.
+ *
+ *		Array2D internally represents a 2D array as a 1D array. Rather than
+ *		an array of pointers to arrays, it is a single array whose row arrays
+ *		are stuck together "end-to-end".
+ *
+ *		You can still index using (row, column). You just have to do math.
+ *		These implementation details are hidden from the user (i.e. you, when
+ *		you're implementing Matrix): if you get it all working here, you don't
+ *		have to worry about any of those things while you're writing Matrix.
+ */
+class Array2D
+{
+	/**
+	 * @brief Type alias for the container that represents an Array2D's size.
+	 */
+	using SizePair = std::pair<size_t, size_t>;
+
+public:
+
+	Array2D() = default;
+
+	/**
+	 * @brief Create a zero-initialized Array2D with the given size.
+	 */
+	Array2D(size_t num_rows, size_t num_cols);
+
+	/**
+	 * @addtogroup BIG_THREE The Big Three
+	 * @brief You have to implement these when working with dynamic memory.
+	 * @{
+	 */
+
+	/**
+	 * @brief Deep-copy the elements of `to_copy` into a new `Array2D`.
+	 * @detail This is a copy constructor.
+	 *
+	 * 		This is the constructor that allows you to:
+	 * 		<ul>
+	 * 		<li> Pass Array2D instances as function arguments that are not
+	 * 			 `const ref`s
+	 * 		<li> Directly construct Array2D's that are duplicates of another
+	 * 			 Array2D by doing: `Array2D copy{another_array_2d};`
+	 * 		</ul>
+	 *
+	 * 		The default (i.e. `... = default;`) copy constructor performs a
+	 * 		"shallow copy". It takes the member variables of the other instance
+	 * 		`to_copy` and copies all of its member variables into a new instance
+	 * 		of that class.
+	 *
+	 * 		This is an issue if your class members include pointers.
+	 * 		Copy-constructing a pointer gives you, in the end, _two_ pointers
+	 * 		that point to the _same location in memory._
+	 *
+	 * 		This means that, if you were to use the default copy constructor
+	 * 		with Array2D, modifications to the _copy_ of Array2D would also
+	 * 		affect the original!
+	 *
+	 * 		You have to deep-copy `to_copy`'s elements instead.
+	 *
+	 * 		To do this, you have to allocate a new `contents` array for the new
+	 * 		Array2D and copy over each element.
+	 */
+	Array2D(const Array2D& to_copy);
+
+	/**
+	 * @brief Assign the contents of `assign_from` to this Array2D.
+	 * @detail This is an assignment operator.
+	 *
+	 * 		This is the function that allows you to do this:
+	 * 				// ... code stuff ...
+	 * 				Array2D an_array; // calls default constructor
+	 * 				an_array = array_from_above; // declared somewhere above
+	 *
+	 * 		Which is functionally identical to:
+	 * 				Array2D an_array = array_from_above;
+	 *
+	 * 		The default assignment operator performs a "shallow copy". See
+	 * 		the documentation for Array2D's copy constructor for an explanation
+	 * 		of why this is bad.
+	 */
+	Array2D& operator=(const Array2D& assign_from);
+
+	/**
+	 * @brief Destroy this Array2D.
+	 * @detail This is a destructor.
+	 *
+	 * 		This is the function that gets called in situations like...
+	 * 				delete ptr_to_dynamically_allocated_array2d;
+	 *
+	 * 		...or, more commonly, in situations like...
+	 * 				void someFunction()
+	 * 				{
+	 *					Array2D an_array;
+	 *
+	 *					// do stuff with array
+	 *
+	 *					return;
+	 * 				}
+	 *
+	 * 		...after you've hit the end of the function.
+	 *
+	 * 		`an_array` no longer exists once the function exits. C++ calls
+	 * 		`delete` on the memory address of `an_array` once you've exited
+	 * 		the body of that function.
+	 *
+	 *		The default destructor `delete`s each of Array2D's member variables.
+	 *		If that member variable was a pointer to dynamically-allocated
+	 *		memory, the `delete` destroys the pointer, but leaves all of that
+	 *		memory still there.
+	 *
+	 *		This is what's called a memory leak.
+	 */
+	~Array2D();
+
+	/**
+	 * @}
+	 */
+
+	/**
+	 * @brief Return the size of this Array as a `(num_rows, num_columns)` pair.
+	 */
+	const SizePair& size() const;
+
+	/**
+	 * @brief Allow modification of a specific element in this Array2D.
+	 * @return A reference to the requested data element. This element can be
+	 * 		modified, which will alter the contents of this Array2D.
+	 */
+	double& operator()(size_t row, size_t col);
+
+	/**
+	 * @brief Allow read-only access to a specific element in this Array2D.
+	 * @return A copy of the value stored at the given index. Altering this
+	 * 		element does not affect this Array2D, hence the `const` cv-qualifier
+	 * 		at the end of this function header.
+	 */
+	double operator()(size_t row, size_t col) const;
+
+	/**
+	 * @brief Allow access to the "raw" array contained by this Array2D.
+	 * @return A reference to the requested data element. This element can be
+	 * 		modified, which will alter the contents of this Array2D.
+	 */
+	double& operator[](size_t index);
+
+	/**
+	 * @brief Allow read-only access to the underlying array.
+	 * @return A copy of the value stored at the given index. Altering this
+	 * 		element does not affect this Array2D, hence the `const` cv-qualifier
+	 * 		at the end of this function header.
+	 */
+	double operator[](size_t index) const;
+
+private:
+
+	/**
+	 * @addtogroup NO_CHANGE Can't Modify These Declarations
+	 * @brief You aren't allowed to modify these variable declarations.
+	 * @detail You will be manipulating the contents of these variables in
+	 * 		your implementation of this class, but you aren't allowed to,
+	 * 		say, replace the `unique_ptr` to an `Array2D` with an
+	 * 		`Eigen::Matrix`.
+	 * @{
+	 */
+
+		/**
+		 * @brief A dynamically-allocated array holding this Array2D's contents.
+		 */
+		double* contents{nullptr};
+
+		/**
+		 * @brief The size of this matrix, given as a `(rows, columns)` pair.
+		 * @detail Zero-initialized, representing an empty Array2D.
+		 */
+		SizePair array_size{0, 0};
+
+	/**
+	 * @}
+	 */
+};
+
+#endif
---
+++ src/Matrix.hpp
@@ -0,0 +1,261 @@
+#ifndef MAAV_PROJECT_3_MATRIX_HPP
+#define MAAV_PROJECT_3_MATRIX_HPP
+
+#include "Array2D.hpp"
+
+#include <iostream>	// std::ostream
+#include <memory>	// std::unique_ptr
+
+/**
+ * @brief Represents a two-dimensional matrix, to be used for linear algebra.
+ * @author Your Name (youruniqname)
+ * @detail Matrix is, essentially, a simplified version of an `Eigen::Matrix`.
+ * 		It's capable of element-wise arithmetic, supports basic matrix math
+ * 		operations, and has a few utility functions to make it easier to use.
+ *
+ * 		Matrix "has-a(n)" Array2D that acts as its internal representation
+ * 		of its contents. This is hidden from the user (i.e. us, when we're
+ * 		writing executables to test your code, and you, when you're writing
+ * 		your test cases).
+ *
+ * 		In the real world, you could change this to something else (maybe you
+ * 		want to just use a native 2D array for sake of simplicity), and as long
+ * 		as you **don't change the public interface** (i.e. as long as row-column
+ * 		indexing still returns the element at the correct row and column, and
+ * 		so on), none of the code that uses `Matrix` would have to change.
+ *
+ * 		(They'll spend a few lectures talking about that in EECS280. This
+ * 		concept is called **procedural abstraction**.)
+ *
+ * 		Note that **all of the methods below** should throw
+ * 		`std::runtime_error`s if invoked upon a "blank" matrix (i.e. a
+ * 		matrix whose `contents` pointer is a `nullptr`).
+ */
+class Matrix
+{
+public:
+
+	/**
+	 * @brief Create a "blank" Matrix.
+	 * @detail This "explicitly defaults" Matrix's default constructor.
+	 */
+	Matrix() = default;
+
+	/**
+	 * @brief Create a zero-initialized Matrix with the given size.
+	 * @detail `size_t` is an unsigned integer type. The `.size()` on
+	 * 		standard library containers return `size_t` values. Ordinarily,
+	 * 		it's an alias for `unsigned long int`, i.e. a 64-bit unsigned
+	 * 		integer.
+	 *
+	 * 		Zero-initialization means that every element of this Matrix will
+	 * 		have a starting value of zero.
+	 */
+	Matrix(size_t num_rows, size_t num_cols);
+
+	/**
+	 * @brief Resize this Matrix to have the given number of rows and columns.
+	 * @return A reference to this matrix (i.e. a dereferenced `this`
+	 * 		pointer, obtained with `(*this)`).
+	 * @detail If the matrix is being shrunk, shrink from the bottom-up
+	 * 		(for rows) and from right-to-left (for columns). This discards
+	 * 		the contents of those rows and columns.
+	 *
+	 * 		If the matrix is being enlarged, zero-initialize the new rows
+	 * 		and columns.
+	 *
+	 * 		Note that, in order to do this, you have to manipulate the
+	 * 		`contents` `Array2D` storing
+	 */
+	Matrix& resize(size_t num_rows, size_t num_cols);
+
+	/**
+	 * @brief Return the element-wise sum of this Matrix with the other.
+	 * @detail Checks whether the matrices have the same size. Throws an
+	 * 		exception (`std::runtime_error`) if the sizes do not match.
+	 *
+	 * 		Passing the following two 2x2 matrices...
+	 * 				[1	2]		[5	6]
+	 * 				[3	4]		[7	8]
+	 *
+	 *		...into this function returns the following matrix:
+	 *				[6	8 ]
+	 *				[10	12]
+	 *
+	 * 		To call this function with `Matrix firstMat` and
+	 * 		`Matrix secondMat` as arguments, do:
+	 * 				Matrix outputMat = firstMat + secondMat;
+	 *
+	 * 		You are overriding the addition operator.
+	 */
+	Matrix operator+(const Matrix& rhs) const;
+
+	/**
+	 * @brief Return the element-wise difference of this Matrix with the other.
+	 * @detail Checks whether the matrices have the same size. Throws an
+	 * 		exception (`std::runtime_error`) if the sizes do not match.
+	 *
+	 * 		Passing the following two 2x2 matrices...
+	 * 				[1	2]		[5	6]
+	 * 				[3	4]		[7	8]
+	 *
+	 *		...into this function returns the following matrix:
+	 *				[-4	-4]
+	 *				[-4	-4]
+	 *
+	 * 		To call this function with `Matrix firstMat` and
+	 * 		`Matrix secondMat` as arguments, do:
+	 * 				Matrix outputMat = firstMat - secondMat;
+	 *
+	 * 		You are overriding the subtraction operator.
+	 */
+	Matrix operator-(const Matrix& rhs) const;
+
+	/**
+	 * @brief Return the matrix-product of these two matrices.
+	 * @detail Checks whether the matrices are appropriately sized. Throws an
+	 * 		exception (`std::runtime_error`) if the sizes are not valid.
+	 *
+	 * 		Passing the following two matrices...
+	 * 				[1	2]		[7	8 ]
+	 * 				[3	4]		[9	10]
+	 * 				[5	6]
+	 *
+	 *		...into this function returns the following matrix:
+	 *				[25	28 ]
+	 *				[57	64 ]
+	 *				[89	100]
+	 *
+	 * 		To call this function with `Matrix firstMat` and
+	 * 		`Matrix secondMat` as arguments, do:
+	 * 				Matrix outputMat = firstMat * secondMat;
+	 *
+	 * 		You are overriding the multiplication operator.
+	 *
+	 * 		For more details on matrix multiplication, use the following
+	 * 		link, reference lecture material, ask a MAAV member for help,
+	 * 		or search for additional resources on Google.
+	 *
+	 * 		[Tutorial](https://www.mathsisfun.com/algebra/matrix-multiplying.html)
+	 */
+	Matrix operator*(const Matrix& rhs) const;
+
+	/**
+	 * @brief Return the element-wise quotient of this Matrix with the other.
+	 * @detail Checks whether the matrices have the same size. Throws an
+	 * 		exception (`std::runtime_error`) if the sizes do not match.
+	 *
+	 * 		Passing the following two 2x2 matrices...
+	 * 				[1	2]		[5	6]
+	 * 				[3	4]		[7	8]
+	 *
+	 *		...into this function returns the following matrix:
+	 *				[0.2	0.333]
+	 *				[0.429	0.5  ]
+	 *
+	 * 		To call this function with `Matrix firstMat` and
+	 * 		`Matrix secondMat` as arguments, do:
+	 * 				Matrix outputMat = firstMat.divide(secondMat);
+	 *
+	 * 		Matrix division is not a well-defined operation. In some
+	 * 		contexts, it refers to finding the inverse of a matrix (`A^1`) and
+	 * 		then multiplying by that inverse (`X * A^1`); other times, it
+	 * 		refers to element-wise division.
+	 */
+	Matrix divide(const Matrix& rhs) const;
+
+	/**
+	 * @brief Return the inverse of this matrix.
+	 * @detail If the matrix is not invertible (i.e. isn't square), throw
+	 * 		an (`std::runtime_error`).
+	 *
+	 * 		The inverse of the 1x1 matrix...
+	 * 				[x]
+	 *
+	 *		...is the 1x1 matrix:
+	 *				[1/x]
+	 *
+	 *		The inverse of the 2x2 matrix...
+	 *				[a	b]
+	 *				[c	d]
+	 *
+	 *		...is given by:
+	 *				(1 / (ad - bc))	*	[ d	-b]
+	 *									[-a	 c]
+	 *
+	 *		**For this function,** you don't need to handle matrices larger
+	 *		than 2x2.
+	 */
+	Matrix inverse() const;
+
+	/**
+	 * @brief Return the transpose of this matrix.
+	 * @detail Return a copy of this matrix, with every element "flipped"
+	 * 		over a diagonal line running from the top-left of the matrix to
+	 * 		its bottom-right.
+	 *
+	 * 		That is, "the rows become columns, and the columns become rows."
+	 *
+	 *		The transpose of the 3x2 matrix `A` (normally denoted `A'`) is
+	 *		given by:
+	 *
+	 *				[a	b]'				[a	c	e]
+	 *				[c	d]		=		[b	d	f]
+	 *				[e	f]
+	 *
+	 *		A "double transpose" (`A''`) is identical to the original
+	 *		matrix (`A'' == A`).
+	 */
+	Matrix transpose() const;
+
+	/**
+	 * @brief Return true when this and the other matrix are equal.
+	 */
+	bool operator==(const Matrix& rhs) const;
+
+	/**
+	 * @brief Return true when this and the other matrix are not equal.
+	 */
+	bool operator!=(const Matrix& rhs) const;
+
+	/**
+	 * @brief Insert the matrix into the given output stream.
+	 * @detail Given a matrix `A`...
+	 * 				[1	2]
+	 * 				[3	4]
+	 *
+	 *		...the function call...
+	 *				std::cout << A << std::endl;
+	 *
+	 *		...would print...
+	 * 				[1	2]
+	 * 				[3	4]
+	 *
+	 * 		...to the terminal window, followed by _one_ newline character.
+	 */
+	std::ostream& operator<<(std::ostream& os) const;
+
+private:
+
+	/**
+	 * @addtogroup NO_CHANGE Can't Modify These Declarations
+	 * @brief You aren't allowed to modify these variable declarations.
+	 * @detail You will be manipulating the contents of these variables in
+	 * 		your implementation of this class, but you aren't allowed to,
+	 * 		say, replace the `unique_ptr` to an `Array2D` with an
+	 * 		`Eigen::Matrix`.
+	 * @{
+	 */
+
+		/**
+		 * @brief The contents of this matrix as a dynamically-allocated array.
+		 */
+		std::unique_ptr<Array2D> contents;
+
+	/**
+	 * @}
+	 */
+
+};
+
+#endif
