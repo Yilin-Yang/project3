14b22f3caf9a1ebfdd8161cea0b0d9b973f2c44e
Yilin Yang <yiliny@umich.edu>
2017-08-28 22:31:05 -0400

    First implementation of maav-equation-solver
    Write maav-equation-solver, which tests some (but not all) of the member
    functions in Matrix. Write Helpers.hpp, which contains helper functions
    used in maav-equation-solver.

---
+++ bin/Helpers.hpp
@@ -0,0 +1,80 @@
+#ifndef MAAV_PROJECT_3_HELPERS_HPP
+#define MAAV_PROJECT_3_HELPERS_HPP
+
+#include "Matrix.hpp"
+
+/**
+ * @addtogroup HELPERS Helper Functions
+ * @brief Useful functions to be used in the main driver.
+ * @{
+ */
+
+/**
+ * @brief Test two doubles for approximate equality.
+ * @return True if values are equal to within a margin of `+/- threshold`,
+ * 		false otherwise.
+ */
+inline bool isApproxEqual(double lhs, double rhs, double threshold)
+{
+	return lhs < rhs + threshold and lhs > rhs - threshold;
+}
+
+/**
+ * @brief Test two matrices for approximate equality.
+ * @return True if all corresponding Matrix elements are equal to within
+ * 		`+/- threshold`, false otherwise. Also return false is matrices have
+ * 		different dimensions.
+ * @param lhs 		The first Matrix to be compared.
+ * @param rhs 		The Matrix against which the first Matrix will be compared.
+ * @param threshold A difference of more than this value is considered unequal.
+ */
+inline bool isApproxEqual(const Matrix& lhs,
+						  const Matrix& rhs,
+						  double threshold = 0.00001)
+{
+	const auto& lhs_size = lhs.size();
+	if (lhs_size != rhs.size()) return false;
+
+	bool are_equal{true};
+
+	// Loop through and compare all elements of both matrices.
+	for (size_t row = 0; row != lhs_size.first; ++row)
+	{
+		for (size_t col = 0; col != lhs_size.second; ++col)
+		{
+			// If the elements are too different...
+			if (not isApproxEqual(lhs(row, col), rhs(row, col), threshold))
+			{
+				are_equal = false;
+
+				// Once we know that *one* element is different, we *know*
+				// that the matrices are unequal, so we exit *both* for-loops.
+				//
+				// NOTE:	A "break" statement would only exit the `col` loop;
+				// 			it wouldn't break out of both.
+
+				goto loop_exit;	// commit coding heresy
+			}
+		}
+	}
+loop_exit:
+	// LEARN:	Why the heresy?
+	// ANSWER:	`goto` statements are traditionally considered bad style
+	// 			(read: heresy) since it's easy to use them in a way that makes
+	// 			your code really ugly and confusing ("spaghetti code").
+	//
+	// 			I think that's generally true, but they're fine if you use
+	// 			them sparingly in situations where it makes sense (like
+	// 			jumping out of nested loops.)
+	//
+	// 			Linux Torvalds, the guy that Linux was named after, agrees:
+	//				https://goo.gl/2y2XQt
+
+	return are_equal;
+}
+
+/**
+ * @}
+ */
+
+#endif
---
+++ bin/maav-equation-solver.cpp
@@ -0,0 +1,255 @@
+#include "Matrix.hpp"
+#include "Helpers.hpp"
+
+#include <functional>
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::function;
+
+void runTest(function<bool(void)> test_function);
+bool solveEquationOne();
+bool solveEquationTwo();
+bool solveEquationThree();
+bool solveEquationFour();
+
+int main()
+{
+	runTest(solveEquationOne);
+	runTest(solveEquationTwo);
+	runTest(solveEquationThree);
+	runTest(solveEquationFour);
+}
+
+/**
+ * @brief Run a test function and report success or failure.
+ * @detail Call a given function and print to stdout whether or not it was
+ * 		successful.
+ * @param test_function	The function to be called.
+ */
+void runTest(function<bool(void)> test_function)
+{
+	cout << "========================================\n";
+	bool test_passed = test_function();	// call test function and store output
+
+	cout << "\n";
+	if (test_passed)
+	{
+		cout << "Test passed!\n";
+	}
+	else
+	{
+		cout << "!!! Test was not successful! !!!\n";
+	}
+	cout << "========================================" << endl;
+}
+
+/**
+ * @brief Solve a linear equation.
+ * @detail Solves the following equation:
+ * 				6 = 2x
+ *
+ * 			... using basic algebra.
+ *
+ * 			The answer is `x = 3`.
+ *
+ * 			Tests the following functions:
+ * 			<ul>
+ * 			<li>	constructor
+ * 			<li>	`divide`
+ * 			<li>	`operator==`
+ * 			<li>	`operator()`
+ * 			</ul>
+ */
+bool solveEquationOne()
+{
+	cout << "TEST01: Solve a single linear equation.\n";
+
+	// right-hand side
+	Matrix a{1, 1};		// coefficient matrix
+	a(1, 1) = 2;
+
+	// left-hand side
+	Matrix b{1, 1};		// "other-side" matrix
+	b(1, 1) = 6;
+
+	Matrix answer{1, 1};
+	answer(1, 1) = 3;
+
+	return b.divide(a) == answer;
+}
+
+/**
+ * @brief Solve a linear equation.
+ * @detail Solves the following equation:
+ * 					7 = 2x + 1
+ *
+ * 			... using basic algebra.
+ *
+ * 			The answer is `x = 3`.
+ *
+ * 			Tests the following functions:
+ * 			<ul>
+ * 			<li>	constructor
+ *			<li>	`operator-`
+ *			<li>	`operator/`
+ *			<li>	`operator=`
+ *			<li>	`operator==`
+ * 			<li>	`operator()`
+ * 			</ul>
+ */
+bool solveEquationTwo()
+{
+	cout << "TEST02: Solve a linear equation with more arithmetic.\n";
+
+	// right-hand side
+	Matrix a{1, 1};		// coefficient matrix
+	a(1, 1) = 2;
+
+	Matrix c{1, 1};		// constants matrix
+	c(1, 1) = 1;
+
+	// left-hand side
+	Matrix b{1, 1};		// "other-side" matrix
+	b(1, 1) = 7;
+
+	// correct answer
+	Matrix answer{1, 1};
+	answer(1, 1) = 3;
+
+	// Attempt to solve the equation.
+	b = b - c;
+	return b.divide(a) == answer;
+}
+
+/**
+ * @brief Solve a simple system of linear equations.
+ * @detail Solves the following system of equations:
+ *					7	= 4x1 + 9x2
+ *					3	= 5x1 + 2x2
+ *
+ *			...by converting the system into a matrix equality of the form:
+ *					Ax 	= B
+ *
+ *			...and multiplying by the inverse matrix to produce the
+ *			following equation:
+ *					x	= (A^-1)B
+ *
+ *			The answer is:
+ *					x1 = 13/37	= 0.351...	(repeating)
+ *					x2 = 23/37	= 0.621...	(repeating)
+ *
+ * 			Tests the following functions:
+ * 			<ul>
+ * 			<li>	constructor
+ * 			<li>	`inverse()`
+ * 			<li>	`operator*`
+ * 			<li>	`operator()`
+ * 			</ul>
+ */
+bool solveEquationThree()
+{
+	cout << "TEST03: Solve a system of equations.\n";
+
+	// right-hand side
+	Matrix a{2, 2};		// coefficient matrix
+	a(1, 1) = 4;
+	a(1, 2) = 9;
+	a(2, 1) = 5;
+	a(2, 2) = 2;
+
+	// left-hand side
+	Matrix b{2, 1};		// "other-side" matrix
+	b(1, 1) = 7;
+	b(2, 1) = 3;
+
+	// correct answer
+	Matrix x_ans{2, 1};
+	x_ans(1, 1) = 13.0 / 37;
+	x_ans(2, 1) = 23.0 / 37;
+
+	// Attempt to solve the equation.
+	return isApproxEqual(b * a.inverse(), x_ans);
+}
+
+/**
+ * @brief Solve a system of linear equations in a roundabout way.
+ * @detail Solves the following system of equations:
+ *					7	= 4x1 + 9x2
+ *					3	= 5x1 + 2x2
+ *
+ *			...by converting the system into a matrix equality of the form:
+ *					Ax 	= B
+ *
+ *			...and multiplying by the inverse matrix to produce the
+ *			following equation:
+ *					x	= (A^-1)B
+ *
+ *			The answer is:
+ *					x1 = 13/37	= 0.351...	(repeating)
+ *					x2 = 23/37	= 0.621...	(repeating)
+ *
+ *			The following complications are added:
+ *			<ul>
+ *			<li>	All matrices start as `1x1` matrices and are resized
+ *					before use.
+ *			<li>	Matrices are populated using a contrived combination
+ *					of `operator=` assignments and copy constructions.
+ *			<li>	All column vectors start as row vectors and are converted
+ *					to column vectors by performing a transpose.
+ *			</ul>
+ *
+ * 			Tests the following functions:
+ * 			<ul>
+ * 			<li>	constructor
+ * 			<li>	copy constructor
+ * 			<li>	`inverse()`
+ * 			<li>	`operator*`
+ * 			<li>	`operator()`
+ * 			<li>	`resize()`
+ * 			<li>	`transpose()`
+ * 			</ul>
+ */
+bool solveEquationFour()
+{
+	cout << "TEST04: Solve a system of equations in a contrived way.\n";
+
+	// right-hand side
+	Matrix a{1, 1};		// coefficient matrix
+	a.resize(2, 2);
+
+	Matrix a_vals{2, 2};// separate matrix containing desired values
+	a_vals(1, 1) = 4;
+	a_vals(1, 2) = 9;
+	a_vals(2, 1) = 5;
+	a_vals(2, 2) = 2;
+
+	a = Matrix{a_vals};	// copy construct a_vals and then assign
+
+	// If the copy-constructor did what it was supposed to, this shouldn't
+	// affect `a` in any way.
+	const auto& val_size = a_vals.size();
+	for (size_t row = 0; row != val_size.first; ++row)
+	{
+		for (size_t col = 0; col != val_size.second; ++col)
+		{
+			a_vals(row, col) = 9001;
+		}
+	}
+
+	// left-hand side
+	Matrix b{1, 2};		// "other-side" matrix, as a row vector
+	b(1, 1) = 7;
+	b(1, 2) = 3;
+
+	b = b.transpose();	// flip it into a column vector
+
+	// correct answer
+	Matrix x_ans{2, 1};
+	x_ans(1, 1) = 13.0 / 37;
+	x_ans(2, 1) = 23.0 / 37;
+
+	// Attempt to solve the equation.
+	return isApproxEqual(b * a.inverse(), x_ans);
+}
